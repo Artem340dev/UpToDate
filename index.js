7c313c9eba73da2120480fa014de0840c4fc6d5d
Artem340dev <artemij.honor@gmail.com>
2023-04-20 21:54:24 +0300

    UpToDate

---
+++ index.js
@@ -0,0 +1,10 @@
+/*console.log($('.main-section-text-container').height())
+
+$(window).scroll(function () {
+	let scrollTop = $(window).scrollTop();
+
+	if (scrollTop < 250) {
+		var currentTop = $('.main-section-text-container').css('top');
+		$('.main-section-text-container').css({'top': '-=10px'});
+	}
+});*/
